07bde328e893da8d392b2fbe42929c6bc078d7e5
Avnish327030 <34875169+Avnish327030@users.noreply.github.com>
2021-02-02 21:40:25 +0530

    messag extracted

--- app.py
+++ app.py
@@ -19,67 +19,10 @@ def index():
     if request.method == 'POST':
         searchString = request.form['content'].replace(" ","") # obtaining the search string entered in the form
         try:
-            ''''
-            dbConn = pymongo.MongoClient("mongodb://localhost:27017/")  # opening a connection to Mongo
-            db = dbConn['crawlerDB'] # connecting to the database called crawlerDB
-            reviews = db[searchString].find({}) # searching the collection with the name same as the keyword
-            
-            if 1==0:#reviews.count() > 0: # if there is a collection with searched keyword and it has records in it
-                return render_template('results.html',reviews=reviews) # show the results to user
-            else:
-                flipkart_url = "https://www.flipkart.com/search?q=" + searchString # preparing the URL to search the product on flipkart
-                uClient = uReq(flipkart_url) # requesting the webpage from the internet
-                flipkartPage = uClient.read() # reading the webpage
-                uClient.close() # closing the connection to the web server
-                flipkart_html = bs(flipkartPage, "html.parser") # parsing the webpage as HTML
-                bigboxes = flipkart_html.findAll("div", {"class": "bhgxx2 col-12-12"}) # seacrhing for appropriate tag to redirect to the product link
-                del bigboxes[0:3] # the first 3 members of the list do not contain relevant information, hence deleting them.
-                box = bigboxes[0] #  taking the first iteration (for demo)
-                productLink = "https://www.flipkart.com" + box.div.div.div.a['href'] # extracting the actual product link
-                prodRes = requests.get(productLink) # getting the product page from server
-                prod_html = bs(prodRes.text, "html.parser") # parsing the product page as HTML
-                commentboxes = prod_html.find_all('div', {'class': "_3nrCtb"}) # finding the HTML section containing the customer comments
-
-                #table = db[searchString] # creating a collection with the same name as search string. Tables and Collections are analogous.
-                #filename = searchString+".csv" #  filename to save the details
-                #fw = open(filename, "w") # creating a local file to save the details
-                #headers = "Product, Customer Name, Rating, Heading, Comment \n" # providing the heading of the columns
-                #fw.write(headers) # writing first the headers to file
-                reviews = [] # initializing an empty list for reviews
-                #  iterating over the comment section to get the details of customer and their comments
-                for commentbox in commentboxes:
-                    try:
-                        name = commentbox.div.div.find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text
-
-                    except:
-                        name = 'No Name'
-
-                    try:
-                        rating = commentbox.div.div.div.div.text
-
-                    except:
-                        rating = 'No Rating'
-
-                    try:
-                        commentHead = commentbox.div.div.div.p.text
-                    except:
-                        commentHead = 'No Comment Heading'
-                    try:
-                        comtag = commentbox.div.div.find_all('div', {'class': ''})
-                        custComment = comtag[0].div.text
-                    except:
-                        custComment = 'No Customer Comment'
-                    #fw.write(searchString+","+name.replace(",", ":")+","+rating + "," + commentHead.replace(",", ":") + "," + custComment.replace(",", ":") + "\n")
-                    mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                              "Comment": custComment} # saving that detail to a dictionary
-                    #x = table.insert_one(mydict) #insertig the dictionary containing the rview comments to the collection
-                    reviews.append(mydict) #  appending the comments to the review list'''
             return render_template('results.html', reviews=feedback(searchString)) # showing the review to the user
         except Exception as e:
-            return e
-
-            #return render_template('results.html')
+            return render_template('index.html',msg={'msg':str(e)})
     else:
-        return render_template('index.html')
+        return render_template('index.html',msg={'msg':None})
 if __name__ == "__main__":
     app.run(port=8000,debug=True) # running the app on the local machine on port 8000
--- filpkartReview.py
+++ filpkartReview.py
@@ -3,24 +3,62 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import  urlopen as ureq
 
 searchString=""
+
+def checkClass(txt):
+    try:
+        return txt["class"]
+    except Exception as e:
+        return ""
+
+
+def getMaxClass(dictionary):
+    name = ""
+    maximum = 0
+    for d in dictionary.keys():
+
+        if maximum < dictionary[d]:
+            print(dictionary[d])
+            maximum = dictionary[d]
+            name = d
+    return name
+
+
+def getClassName(anchortags):
+    try:
+        count = 0
+        previous_class = {}
+        for i in anchortags:
+            if checkClass(i) != "":
+                if i["class"][0] in previous_class:
+                    previous_class[i["class"][0]] = previous_class[i["class"][0]] + 1
+
+                else:
+                    previous_class[i["class"][0]] = 1
+
+
+        return getMaxClass(previous_class)
+    except Exception as e:
+        raise Exception("not able to find class name --->" + str(e))
+
+
 def getFLipkartpage(url):
     try:
         page = ureq(url).read()
-
         return page
     except Exception as e:
-        raise Exception("Not able to retrive flipkart page")
+        raise Exception("Not able to retrive flipkart page--->")
 
 
 def getBigBoxes(html_page):
     try:
-        boxes = html_page.find_all("a", {"class": "IRpwTa"})
+        class_name=getClassName(html_page.find_all('a'))
+        boxes=html_page.find_all('a', {'class': class_name})
         if len(boxes) > 0:
             return boxes
         else:
             raise Exception("no boxes found to search for comment")
     except Exception as e:
-        raise Exception("Exception occured in getBigBoxesfunction" + str(e))
+        raise Exception("Exception occured in getBigBoxesfunction--->" + str(e))
 
 
 def generateURLForProduct(boxes):
@@ -38,48 +76,70 @@ def generateURLForProduct(boxes):
         if len(url_list) > 0:
             return url_list
         else:
-            raise Exception("not able to generate URL for product" + str(e))
+            raise Exception("not able to generate URL for product--->" + str(e))
 
 
 def getFeebackofProduct(url):
-    loop_time = 6
-    i = 1
-    isPageNoCalculated = False
-    feedback = []
-    while i < loop_time:
-        prodRes = requests.get(url + str(i))
-        i = i + 1
-        # print(prodRes.text)
-        prod_html = bs(prodRes.text, "html.parser")
-        res = prod_html.find_all('div', {'class': '_6K-7Co'})
-        rating = prod_html.find_all('div', {'class': '_3LWZlK _1BLPMq _3B8WaH'})
-        name = prod_html.find_all('p', {'class': '_2sc7ZR _2V5EHH _1QgsS5'})
+    try:
+        loop_time = 6
+        i = 1
+        isPageNoCalculated = False
+        feedback = []
+        while i < loop_time:
+            prodRes = requests.get(url + str(i))
+            i = i + 1
+            prod_html = bs(prodRes.text, "html.parser")
+            res = prod_html.find_all('div', {'class': '_6K-7Co'})
+            if len(res)<2:
+                res=[i.div.div for i in prod_html.find_all('div', {'class': '_27M-vq'})]
 
-        product_name = prod_html.find_all('div', {'class': '_2s4DIt _1PEOhe'})
-        if len(product_name) > 0:
-            product_name = product_name[0].text
-        page_no = prod_html.find_all('div', {'class': '_2MImiq _1Qnn1K'})
-        if len(page_no) > 0 and isPageNoCalculated == False:
-            page_detail = page_no[0].span.text
-            loop_time = int(page_detail[page_detail.index("of ") + 3:])
-            isPageNoCalculated = True
-            if loop_time > 10:
-                loop_time = 10
-            print(loop_time)
-        for r in range(0, min([len(res), len(rating)])):
-            feedback.append(
-                {"rating": rating[r].text, "comment": res[r].text, "name": name[r].text, "product Name": product_name,
-                 "product searched": searchString})
-    return feedback
+            rating = prod_html.find_all('div', {'class': '_3LWZlK _1BLPMq _3B8WaH'})
+            if len(rating)<1:
+                rating = prod_html.find_all('div', {'class': '_3LWZlK _1BLPMq'})
+
+            name = prod_html.find_all('p', {'class': '_2sc7ZR _2V5EHH _1QgsS5'})
+            if len(name)<1:
+                name = prod_html.find_all('p', {'class': '_2sc7ZR _2V5EHH'})
+
+
+            product_name = prod_html.find_all('div', {'class': '_2s4DIt _1PEOhe'})
+            if len(product_name) > 0:
+                product_name = product_name[0].text
+            else:
+                product_name = prod_html.find_all('div', {'class': '_2s4DIt'})
+                if len(product_name) > 0:
+                    product_name = product_name[0].text
+
+            page_no = prod_html.find_all('div', {'class': '_2MImiq _1Qnn1K'})
+            if len(page_no) > 0 and isPageNoCalculated == False:
+                page_detail = page_no[0].span.text
+                loop_time = int(page_detail[page_detail.index("of ") + 3:])
+                isPageNoCalculated = True
+                if loop_time > 10:
+                    loop_time = 10
+
+
+            for r in range(0, min([len(res), len(rating)])):
+                feedback.append(
+                    {"rating": rating[r].text, "comment": res[r].text, "name": name[r].text, "product Name": product_name,
+                    "product searched": searchString})
+        return feedback
+    except Exception as e:
+        raise Exception("Error in getFeebackofProduct method---->"+str(e))
 
 
 def feedback(product_name):
-    global searchString
-    searchString=product_name
-    flipkart_url = "https://www.flipkart.com/search?q=" + product_name
-    flipkart_page = getFLipkartpage(flipkart_url)
-    flipkart_html = bs(flipkart_page, "html.parser")
-    bigboxes = getBigBoxes(flipkart_html)
-    urls = generateURLForProduct(bigboxes)
-    return getFeebackofProduct(urls[0])
+    try:
+        global searchString
+        searchString=product_name
+        flipkart_url = "https://www.flipkart.com/search?q=" + product_name
+        flipkart_page = getFLipkartpage(flipkart_url)
+        flipkart_html = bs(flipkart_page, "html.parser")
+        bigboxes = getBigBoxes(flipkart_html)
+        urls = generateURLForProduct(bigboxes)
+        return getFeebackofProduct(urls[0])
+    except Exception as e:
+        raise Exception("Error in feedback method  --->"+str(e))
+
+
 
